d2951e7a31ed5a09d76d707051a1ed338cfdaa61
mrnuggets699 <95525494+mrnuggets699@users.noreply.github.com>
2021-12-28 14:25:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.PREFIX = "n//";
-exports.OWNER_ID = "532177714203852800";
-exports.Owner_Name = "Saizuo#0007";
+exports.PREFIX = ".";
+exports.OWNER_ID = "706822316918833184";
+exports.Owner_Name = "mrnuggets#9881";
